e65b01cff2c2822ed60b5d9d7f3cae22ac11afa4
dinodniluksha <dinodniluksha@gmail.com>
2022-01-21 23:57:10 +0530

    Completed adding Apollo client & GraphQl API integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Subscription } from 'rxjs';
+
+const GET_USERS = gql`
+  query {
+  getUsers {
+    id
+    login
+    avatar_url
+  }
+}`;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tour-mate-admin';
+
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'angular-graphql';
+  loading: boolean = true;
+  users: any;
+
+  querySubscription: Subscription = new Subscription;
+  constructor(private apollo: Apollo) { }
+
+  ngOnInit(): void {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: GET_USERS
+    })
+      .valueChanges
+      .subscribe(({ data, loading }) => {
+        this.loading = loading;
+        this.users = data.getUsers;
+        console.log(this.users);
+        console.log(this.loading);
+      });
+  }
+
+  ngOnDestroy() {
+    this.querySubscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
