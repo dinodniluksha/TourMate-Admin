3d68ae57c138e72a5aca8a90280f4df52cfceb3e
dinodniluksha <dinodniluksha@gmail.com>
2022-01-24 21:18:35 +0530

    Added material buttons & loading spinner

--- src/app/admin-dashboard/vehicle-gallery/vehicle-gallery.component.ts
+++ src/app/admin-dashboard/vehicle-gallery/vehicle-gallery.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
 import { Subscription } from 'rxjs';
+import { NgxSpinnerService } from "ngx-spinner";
 
 const GET_VEHICLES = gql`
   query {
@@ -25,9 +26,10 @@ export class VehicleGalleryComponent implements OnInit, OnDestroy {
   vehicles: any;
   querySubscription: Subscription = new Subscription;
 
-  constructor(private apollo: Apollo) { }
+  constructor(private apollo: Apollo, private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.spinner.show();
     this.querySubscription = this.apollo.watchQuery<any>({
       query: GET_VEHICLES
     }).valueChanges
